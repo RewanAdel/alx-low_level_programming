e304cc2a938747a58687968acc2c556e81c9719d
RewanAdel <rewanadel8008@gmail.com>
2023-03-16 13:26:54 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * a C program that prints using the function puts
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+puts("\""Programming is like building a multilingual puzzle");
+return (0);
+}
