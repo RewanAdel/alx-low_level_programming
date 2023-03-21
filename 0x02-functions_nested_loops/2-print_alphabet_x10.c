8501c64b16ddad83ba2b29d0b81da85d85b257bc
RewanAdel <rewanadel8008@gmail.com>
2023-03-21 11:26:14 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char x = 'a';
+
+	while (round < 10)
+	{
+		x = 'a';
+		while (x <= 'z')
+		{
+			_putchar(x);
+			x++;
+		}
+		_putchar('\n');
+
+		round++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char x);
 void print_alphabet(void);
 int _islower(int c);
 int _isalpha(int c);
+void print_alphabet_x10(void);
