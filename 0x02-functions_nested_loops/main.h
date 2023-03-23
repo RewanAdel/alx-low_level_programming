76e374cf0766862cb67d4cfb495d89238a8867b4
RewanAdel <rewanadel8008@gmail.com>
2023-03-22 17:41:49 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 int _putchar(char x);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-void print_alphabeti_x10(void);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
