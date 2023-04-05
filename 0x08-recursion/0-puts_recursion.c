b06a080b69fdd844645bd7aa97c3d67f0ba7b1e0
RewanAdel <rewanadel8008@gmail.com>
2023-04-04 23:52:05 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ *
+ * @s: input string
+ *
+ * Return: pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+	else
+	{
+		_putchar('\n');
+		return;
+	}
+}
