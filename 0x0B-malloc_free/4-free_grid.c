64346d2e426bda68e2cab297abb48a8b501399dc
RewanAdel <rewanadel8008@gmail.com>
2023-04-11 20:39:03 -0700

    free_grid.c: function that frees a 2D grid previously created

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - allocates a grid, make space and free space
+ *
+ * @grid: width of grid
+ * @height: height of grid
+ *
+ * Return: free grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int x;
+
+	for (x = 0; x < height; x++)
+	{
+		free(grid[x]);
+	}
+	free(grid);
+}
