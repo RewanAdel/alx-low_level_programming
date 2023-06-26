8339d2da9fdbedc5f4cf9350c8dadbea5713b6a4
RewanAdel <rewanadel8008@gmail.com>
2023-06-26 11:27:21 -0700

    print

---
+++ 0x0000-print/main.h
@@ -0,0 +1,5 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
---
+++ 0x0000-print/printt.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _printf - prints output according to format
+ * @format: string being passed
+ * Return: char to be printed
+ */
+
+int _printf(const char *format, ...)
+{
+   va_list args;
+   int count = 0;
+
+   va_start(args, format);
+
+   while (*format != '\0') {
+      if (*format == '%') {
+         format++;
+         switch (*format) {
+            case 'c':
+               count += printf("%c", va_arg(args, int));
+               break;
+            case 's':
+               count += printf("%s", va_arg(args, char *));
+               break;
+            case '%':
+               count += printf("%%");
+               break;
+            case 'd':
+            case 'i':
+               count += printf("%d", va_arg(args, int));
+               break;
+            default:
+               break;
+         }
+      } else {
+         putchar(*format);
+         count++;
+      }
+      format++;
+   }
+
+   va_end(args);
+
+   return count;
+}
