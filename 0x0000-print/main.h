a4853f807a7c33f6de901d348d73c2a4d185ee34
RewanAdel <rewanadel8008@gmail.com>
2023-06-27 16:04:41 -0700

    update

--- 0x0000-print/main.h
+++ 0x0000-print/main.h
@@ -9,34 +9,10 @@
 
 #define BUFF_SIZE 1024
 
-/**
-* struct fun - checks formats
-* @z: format
-* @y: function
-*/
-struct fun
-{
-	char z;
-	int (*y)(va_list);
-};
-
-/**
-* typedef struct fun so - struct definition
-* @fun: formats to use
-* @so: function
-*/
-
-typedef struct fun so;
-
 int _printf(const char *form, ...);
 int _strlen(char *s);
-
-/* Funtions to print chars and strings */
-int handle_print(const char *format, int *i, va_list list);
 int ch(va_list z);
 int str(va_list k);
 int _putchar(char c);
-int print_37(va_list list);
-/* Functions to print numbers */
-int print_int(va_list list);
-#endif
+
+#endif /* MAIN_H */
