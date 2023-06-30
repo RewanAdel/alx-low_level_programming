b664fece36155ab64db591b7862030e97a709a12
RewanAdel <rewanadel8008@gmail.com>
2023-06-30 16:37:04 -0700

    Add node at the end

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,35 @@
 #include "lists.h"
 
 /**
+ * _strlen - returns a string lengt
+ * @s: string to check
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	if (!s)
+		return (0);
+	while (*s++)
+		x++;
+	return (x);
+}
+
+/**
  * print_list - prints all the elements of a list
  * @h: the linked list head
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t x;
+	size_t x = 0;
 
-	x = 0;
-	while (h != NULL)
+	while (h)
 	{
-		x++;
-		if (h->str == NULL)
-			printf("[%d] (nil)\n", 0);
-		else
-			printf("[%d] %s\n", h->len, h->str);
+		printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
 		h = h->next;
+		x++;
 	}
 	return (x);
 }
