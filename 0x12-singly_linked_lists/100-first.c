fbdc85f013af3b2005af8f515dd24d1647014532
RewanAdel <rewanadel8008@gmail.com>
2023-06-30 17:00:38 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print - print before the main function
+ * Return: void
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+	printf("my back!\n");
+}
